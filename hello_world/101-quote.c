082f9d9e69eaf37ffa31b3a9c7772870904e2559
normancolon90 <6848@holbertonstudents.com>
2023-06-16 16:19:20 +0000

    /*

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,10 +1,16 @@
 /*
  * File: 101-quote.c
- * Auth: N C
+ * Auth: Norman Colon
  */
 
 #include <unistd.h>
 
+/**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+ *
+ * Return: Always 1.
+ */
 int main(void)
 {
 	write(2,
