222e70d048c9c1ead4adba12c99df823caea38d9
Felipe Santos <felipecassiors@gmail.com>
2023-06-19 18:01:15 +0000

    9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -17,13 +17,11 @@ int main(void)
     {
         putchar((num % 10) + '0');
         if (num == 9)
-            continue;
-
+        {
         putchar(',');
         putchar(' ');
     }
-
+    }
     putchar('\n');
-
     return (0);
 }
