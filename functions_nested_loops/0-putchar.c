516040df844d4b0bb7bdcdade54a86e80d7bcd93
Felipe Santos <felipecassiors@gmail.com>
2023-06-20 17:57:03 +0000

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,7 +1,8 @@
 #include "main.h"
+
 /**
  * main -  prints _putchar, followed by a new line
- * If it Return: always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -14,5 +15,7 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
+
