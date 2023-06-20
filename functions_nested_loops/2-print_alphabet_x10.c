6cbd7eeb3f7e313905512cedb14a1e457b8c7feb
Felipe Santos <felipecassiors@gmail.com>
2023-06-20 18:32:27 +0000

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -2,21 +2,4 @@
  * File: 2-print_alphabet_x10.c
  * Norman C
  */
-#include main.h
-void print_alphabet_x10(void)
-{
-	char alphabet = 'a';
-	int counter = 0;
 
-	while (counter < 10)
-	{
-		while (alphabet < 123)
-		{
-		    _putchar(alphabet);
-			alphabet++;
-		}
-	alphabet = 'a';
-	_putchar(10);
-	counter++;
-	}
-}
