2f0ddccfbfde38176482eb3a38a9ec79b365c752
Felipe Santos <felipecassiors@gmail.com>
2023-06-20 18:02:43 +0000

    /*

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,2 +1,32 @@
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
 int _putchar(char c);
+
+int _isalpha(int c);
+
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int print_sign(int n);
+
+int _abs(int a);
+
+void print_to_98(int n);
+
+int print_last_digit(int a);
+
+void print_to_98(int n);
+
+int add(int a, int b);
+
+void jack_bauer(void);
+
+void times_table(void);
 
