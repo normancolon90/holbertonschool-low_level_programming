b7ee7a0f85d4f786965b296c77c8cd13814cbdea
Felipe Santos <felipecassiors@gmail.com>
2023-06-21 13:11:52 +0000

    8-24_hours.c

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
+ * jack_bauer -  prints every minute of the day of Jack Bauer.
  * Author: Norm
- *  prints every minute of the day of Jack Bauer.
  *
  * Return: void
  */
