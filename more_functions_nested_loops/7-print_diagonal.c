44ebecce1561508086d5f35311b81c93e9ae89c6
Felipe Santos <felipecassiors@gmail.com>
2023-06-22 18:58:38 +0000

    7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -16,9 +16,9 @@ void print_diagonal(int n)
 		{
 			for (spaces = 0; spaces < i; spaces++)
 			{
-		_putchar(' ');
+				_putchar(' ');
 			}
-			_putchar(92);
+			_putchar('\\');
 			_putchar('\n');
 		}
 	}
