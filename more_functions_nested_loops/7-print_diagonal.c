5fe3779944525a4ff88deb0960af9cc0c9d1acc7
Felipe Santos <felipecassiors@gmail.com>
2023-06-22 18:25:38 +0000

    6-print_line.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints \ alot
+ * @n: number of dashes
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i, spaces;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (spaces = 0; spaces < i; spaces++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+	else
+	_putchar('\n');
+}
