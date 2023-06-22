cf6019897a2f58af0d75b00bfd3ff168b8231f9c
Felipe Santos <felipecassiors@gmail.com>
2023-06-22 18:21:44 +0000

    4-print_most_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code for school School students.
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
+}
