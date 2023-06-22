73e663dcb5540c84f90968e1faed6c28d63b033d
Felipe Santos <felipecassiors@gmail.com>
2023-06-22 18:15:31 +0000

    2-mul.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if character is alphabetic uppercase
+ * @c: character being passed
+ *
+ * Return: 1 if Uppercase. Else 0.
+ */
+int _isupper(int c)
+{
+	if (c < 91 && c > 64)
+		return (1);
+	else
+		return (0);
+}
+
---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+
+#include "main.h"
+
+/**
+ * mul - takes two integer arguments and multiplies them together.
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the result;
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
